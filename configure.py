630a2ae7bc68264e2d90d311f09a701f0691c8a7
Angus Ainslie <angus@akkea.ca>
2014-11-19 22:09:11 -0700

    * Add command line switches for configuration

--- configure.py
+++ configure.py
@@ -5,30 +5,92 @@
 #
 import serial
 import array
+import argparse
+import sys
 
-port = '/dev/ttyUSB0'
-baud = 9600
-callsign = '      1'
-icon = '6'
-freq = '144.3900'
-update_rate = '0900'
-ptt_delay = '6'
-path = '2'
-tx_mode = '5'
-beep = '0'
-digi = '11'
-comment = "Comment"
-status =  "Status"
+parser = argparse.ArgumentParser(description='Configure the ap510 aprs device.')
+parser.add_argument('-p', '--port',  dest='port',
+        help='serial port for the ap510', default='/dev/ttyUSB0' )
+parser.add_argument('-b', '--baud', type=int, dest='baud',
+        help='baud rate for ap510', default=9600)
+parser.add_argument('-v', '--verbose', type=int, dest='verbose',
+        help='debug level', default=0)
+parser.add_argument('-c', '--callsign', dest='callsign',
+        help='call sign for outgoing aprs messages', default='      ')
+parser.add_argument('-s', '--ssid', type=int, dest='ssid',
+        help='SSID to append to callsign', default=0)
+parser.add_argument('-i', '--icon', type=int, dest='icon',
+        help='aprs icon', default=6)
+parser.add_argument('-f', '--freq', dest='freq',
+        help='aprs frequency', default='144.3900')
+parser.add_argument('-u', '--update', dest='update_rate', type=int,
+        help='time between automatic aprs TX', default=900)
+parser.add_argument('-d', '--delay', dest='ptt_delay', choices=range(1,8),
+        help='time to assert PTT before TX    1 - 60 ms, 2 - 120 ms, 3 - 180 ms,\
+                 4 - 300 ms, 5 - 480 ms, 6 - 600 ms, 7 - 1000 ms', default=6)
+parser.add_argument('-S', '--show', dest='show',
+        help='print the current config', default=False, action='store_true')
+parser.add_argument('-e', '--exit', dest='exit',
+        help='exit after config', default=False, action='store_true')
+parser.add_argument('-P', '--path', dest='path', type=int, choices=range(0,7),
+        help='0 - none, 1 - "WIDE 1-1", 2 - "WINE 1-1 WIDE 2-1", 3 - "WINE 1-1 WIDE 2-2", \
+        4 - "TEMP 1-1", 5 - "TEMP 1-1 WIDE 2-1", 6 - "WIDE 2-1"', default=1 )
+parser.add_argument('-t', '--txmode', dest='tx_mode', type=int, choices=range(1,6),
+        help='1 manual, 2 auto, 3 manual + auto, 4 smartbeacon, 5 smartbeacon + manual'
+        , default=1 )
+parser.add_argument('-B', '--beep', dest='beep', type=int, choices=range(0,2),
+        help='0 - off, 1 - on ', default=0 )
+parser.add_argument('-D', '--digi', dest='digi', choices=[ '00','11','12','13' ],
+        help='00 - no digipeat, 11 - wide 1, 12 - wide 2, 13 - wide 1 + wide 2', default='00' )
+parser.add_argument('-C', '--comment', dest='comment',
+        help='comment field for the aprs packet', default="Comment" )
+parser.add_argument( '--status', dest='status',
+        help='status field for the aprs packet', default="Status" )
+parser.add_argument( '--bls', dest='bls', type=int,
+        help='SMARTBeacon low speed', default=50 )
+parser.add_argument( '--bhs', dest='bhs', type=int,
+        help='SMARTBeacon high speed', default=100 )
+parser.add_argument( '--bsr', dest='bsr', type=int,
+        help='SMARTBeacon slow rate', default=180 )
+parser.add_argument( '--bfr', dest='bfr', type=int,
+        help='SMARTBeacon fast rate', default=180 )
+parser.add_argument( '--bts', dest='bts', type=int,
+        help='SMARTBeacon turn slope', default=45 )
+parser.add_argument( '--bta', dest='bta', type=int,
+        help='SMARTBeacon turn angle', default=45 )
+parser.add_argument( '--btt', dest='btt', type=int,
+        help='SMARTBeacon turn time', default=15 )
 
-lowSpeed = 50
-hispeed = 100
-slowRate = 180
-fastRate = 60
-turnSlope = 45
-turnAngle = 45
-turnTime = 15
+args = parser.parse_args()
+if args.verbose > 0 :
+    print args
 
-ser = serial.Serial( port, baud, timeout=0.5)
+def usage() :
+    parser.print_help()
+    sys.exit()
+
+if len( args.callsign ) > 6 :
+    usage()
+
+callsign = args.callsign + "{:1x}".format( args.ssid ) + "{:1x}".format( args.icon )
+update_rate = "{:04d}".format( args.update_rate )
+ptt_delay = "{:1d}".format( args.ptt_delay )
+path = "{:1d}".format( args.path )
+tx_mode = "{:1d}".format( args.tx_mode )
+beep = "{:1d}".format( args.beep )
+digi = args.digi
+comment = args.comment
+status = args.status
+
+lowSpeed = args.bls
+hispeed = args.bhs
+slowRate = args.bsr
+fastRate = args.bfr
+turnSlope = args.bts
+turnAngle = args.bta
+turnTime = args.btt
+
+ser = serial.Serial( args.port, args.baud, timeout=0.5)
 
 done = False
 
@@ -75,8 +137,11 @@ def display():
         if '18=' in line :
             print "hex: " + ":".join("{:02x}".format( ord( c )) for c in line )
 
+if len ( args.freq ) < 8 :
+    usage()
+
 data = [
-    ( "Callsign", "@01" + callsign + icon),
+    ( "Callsign", "@01" + callsign ),
     ( "PTT delay", "@02" + ptt_delay),
     ( "Path", "@05" + path),
     ( "TX mode", "@07" + tx_mode),
@@ -84,12 +149,13 @@ data = [
     ( "Comment", "@09" + comment + "\n"),
     ( "Status", "@10" + status + "\n"),
     ( "Digipeat", "@12" + digi + "\n"),
-    ( "Frequency", "@16" + freq),
+    ( "Frequency", "@16" + args.freq),
     ( "beep", "@17" + beep ),
     ( "Smart Beacon", "@18" + smartEnc() ),
 ]
 
-display()
+if args.show :
+    display()
 
 for line in data:
     print "Writing %d : %s %s  " %( len( line[1] ), line[0], line[1] )
@@ -97,7 +163,9 @@ for line in data:
     s = ser.read(500)
     print "Return :", s
 
+print "New Config"
 display()
 
-print "exit"
-ser.write( "@EXIT" )
+if args.exit :
+    print "exit"
+    ser.write( "@EXIT" )
